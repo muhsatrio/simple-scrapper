302eef727970ab3a32c82f2c38ca9841cd19fcb4
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-07-23 11:12:43 +0700

    fixing solution.js

--- solution.js
+++ solution.js
@@ -2,12 +2,12 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-var parsingResult = [];
-var page;
-var count = 0;
+let parsingResult = [];
+let page;
+let count = 0;
 
-for (var i=0;i<6;i++) {
-    for (var j=0;j<15;j++) {
+for (let i=0;i<6;i++) {
+    for (let j=0;j<15;j++) {
         parsingResult[count] = new Promise((resolve, reject) => {
             request(`https://www.bankmega.com/promolainnya.php?product=0&subcat=${i+1}&page=${j+1}`, (err, res, html) => {        
                if (!err) {
@@ -47,53 +47,26 @@ Promise.all([parsingResult[0], parsingResult[1], parsingResult[2], parsingResult
     parsingResult[83], parsingResult[84], parsingResult[85], parsingResult[86], parsingResult[87], parsingResult[88],
     parsingResult[89]
             ]).then(values => {
-    // var result = [];
-    var result_1 = [];
-    var result_2 = [];    
-    var result_3 = [];
-    var result_4 = [];
-    var result_5 = [];
-    var result_6 = [];
-    var count = 0;
-    for (var i=0;i<6;i++) {
-        for (var j=0;j<15;j++) {
-            if (i==0) 
-                result_1.push.apply(result_1, values[count]);
-            else if (i==1) 
-                result_2.push.apply(result_2, values[count]);
-            else if (i==2) 
-                result_3.push.apply(result_3, values[count]);
-            else if (i==3) 
-                result_4.push.apply(result_4, values[count]);
-            else if (i==4) 
-                result_5.push.apply(result_5, values[count]);
-            else if (i==5) 
-                result_6.push.apply(result_6, values[count]);
+    let list_data = [[], [], [], [], [], []];
+    let count = 0;
+    for (let i=0;i<6;i++) {
+        list_data[i].push([])
+        for (let j=0;j<15;j++) {
+            list_data[i].push.apply(list_data[i], values[count]);
             count++;
         }
     }
-    var result = JSON.stringify({
-        travel_and_entertainment: result_1, 
-        lifestyle_and_wellness: result_2,
-        food_and_baverage: result_3,
-        gadget_and_electronics: result_4,
-        daily_needs_and_home_appliances: result_5,
-        other: result_6, 
+    const result = JSON.stringify({
+        travel_and_entertainment: list_data[0], 
+        lifestyle_and_wellness: list_data[1],
+        food_and_baverage: list_data[2],
+        gadget_and_electronics: list_data[3],
+        daily_needs_and_home_appliances: list_data[4],
+        other: list_data[5], 
     });
     fs.writeFile ("solution.json", result, function(err) {
         if (err) throw err;
         console.log('solution.json created');
         }
     );
-    // console.log(result);
-    // console.log(result_1.length);
-    // console.log(result_2.length);
-    // console.log(result_3.length);
-    // console.log(result_4.length);
-    // console.log(result_5.length);
-    // console.log(result_6.length);
-    // for (var i=0;i<30;i++) {
-    //     result.push.apply(result, values[i]);
-    // }
-    // console.log(result);
 });
