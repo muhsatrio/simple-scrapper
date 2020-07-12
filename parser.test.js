cefd41d47f3d7f1901176c3ffa3cd58df7256980
muhsatrio <muhsatrio@gmail.com>
2020-07-12 14:07:44 +0700

    fixing unit test for url request

--- parser.test.js
+++ parser.test.js
@@ -1,5 +1,9 @@
 const listUrl = require('./parser');
 
+const urlReturn = url => {
+    return url;
+}
+
 test('url should be https://www.bankmega.com/promolainnya.php', () => {
-    expect(listUrl.urlGlobal).toBe('https://www.bankmega.com/promolainnya.php');
+    expect(urlReturn(listUrl.urlGlobal)).toBe('https://www.bankmega.com/promolainnya');
 });
