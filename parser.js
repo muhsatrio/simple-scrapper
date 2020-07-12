19d5ede776d41b039b379968488c8ed646de6c71
muhsatrio <muhsatrio@gmail.com>
2020-07-12 08:02:46 +0700

    fix parser to more clean code

---
+++ parser.js
@@ -0,0 +1,51 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const rawCategories = [
+    'Travel_and Entertainment',
+    'Lifestyle and Wellness',
+    'Food and Baverage',
+    'Gadget and Electronics',
+    'Daily Needs and Home Appliances',
+    'Other'
+]
+
+const parsingData = async (subcategory, page) => {
+    const parseResult = await request(`https://www.bankmega.com/promolainnya.php?product=0&subcat=${subcategory}&page=${page}`);
+    let dataList = []
+    const $ = cheerio.load(parseResult);
+    const resultData = $('#imgClass');
+    if (resultData.length>0) {
+        resultData.each(function(i, val) {
+            dataList = [...dataList, {
+                title: $(this).attr('title'),
+                imgurl: $(this).attr('src')
+            }];
+        });
+    }
+    return dataList;
+}
+
+const mainProgram = async () => {
+    const promiseParse = rawCategories.map(async (eachCategory, numCategory) => {
+        const promisePages = pages.map(async (eachPage) => {
+            const resultParse = await parsingData(numCategory+1, eachPage);
+            return resultParse;
+        });
+        const resultData = await Promise.all(promisePages);
+        return {
+            category: eachCategory,
+            data: resultData
+        };
+    })
+    const results = await Promise.all(promiseParse);
+    fs.writeFile ("solution.json", JSON.stringify(results), (err) => {
+        if (err) throw err;
+        console.log('solution.json created');
+        }
+    );
+}
+
+mainProgram();
--- test.js
+++
@@ -1,6 +0,0 @@
-let list_data = [];
-
-list_data.push([1, 2]);
-list_data.push([3, 4]);
-
-console.log(list_data[0][0]);
